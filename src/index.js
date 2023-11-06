2385d7f5e102ec760dc81269b3f8b9b8a4b0b4ca
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2023-11-06 07:16:36 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,55 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+// import { kv } from "@vercel/kv";
+
+require('dotenv').config();
+const middlewares = require('./middlewares');
+const api = require('./api');
+
 const app = express();
-const port = 3000; // You can change this to your desired port
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  let page = req.query.page;
+  try {
+    // Make a fetch request to an external API
+    const apiUrl = 'https://caring-fawn-44928.upstash.io/get/user';
+    const fetchOptions = {
+      headers: {
+        Authorization: "Bearer Aa-AASQgNTkwY2Q2NjAtN2FjZC00MzdhLWI4ZDQtZDQwMmQxOGJjODNjZTA0YjkzZDU1MDIyNDQyNDliN2QwNjE5NWQxOTI5NDA="
+      }
+    };
+
+    fetch("https://caring-fawn-44928.upstash.io/get/"+page, {
+      headers: {
+        Authorization: "Bearer Aa-AASQgNTkwY2Q2NjAtN2FjZC00MzdhLWI4ZDQtZDQwMmQxOGJjODNjZTA0YjkzZDU1MDIyNDQyNDliN2QwNjE5NWQxOTI1NDA="
+      }
+    }).then(response => response.json())
+      .then(data => res.json(data.result));
+
+
+    const response = await fetch(apiUrl, fetchOptions);
+    if (!response.ok) {
+      console.log(JSON.stringify(response.result))
+    }
+
+  } catch (error) {
+    // Handle any errors that occur during the fetch or JSON parsing
+    res.status(500).json({ error: error.message });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.use('/api/v1', api);
+
+
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+module.exports = app;
