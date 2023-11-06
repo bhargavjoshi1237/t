5607b4761049a15f4b7365dc54db7bd93409cdc6
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2023-11-06 08:27:45 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,55 +1,8 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
-// import { kv } from "@vercel/kv";
+const app = require('./app');
 
-require('dotenv').config();
-const middlewares = require('./middlewares');
-const api = require('./api');
-
-const app = express();
-
-app.use(morgan('dev'));
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-
-app.get('/', async (req, res) => {
-  let page = req.query.page;
-  try {
-    // Make a fetch request to an external API
-    const apiUrl = 'https://caring-fawn-44928.upstash.io/get/user';
-    const fetchOptions = {
-      headers: {
-        Authorization: "Bearer Aa-AASQgNTkwY2Q2NjAtN2FjZC00MzdhLWI4ZDQtZDQwMmQxOGJjODNjZTA0YjkzZDU1MDIyNDQyNDliN2QwNjE5NWQxOTI5NDA="
-      }
-    };
-
-    fetch("https://caring-fawn-44928.upstash.io/get/"+page, {
-      headers: {
-        Authorization: "Bearer Aa-AASQgNTkwY2Q2NjAtN2FjZC00MzdhLWI4ZDQtZDQwMmQxOGJjODNjZTA0YjkzZDU1MDIyNDQyNDliN2QwNjE5NWQxOTI1NDA="
-      }
-    }).then(response => response.json())
-      .then(data => res.json(data.result));
-
-
-    const response = await fetch(apiUrl, fetchOptions);
-    if (!response.ok) {
-      console.log(JSON.stringify(response.result))
-    }
-
-  } catch (error) {
-    // Handle any errors that occur during the fetch or JSON parsing
-    res.status(500).json({ error: error.message });
-  }
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
 });
-
-app.use('/api/v1', api);
-
-
-
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
-
-module.exports = app;
