20048bec7888fe0251bd46b89bdd2666ea3c0688
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2023-11-14 12:37:23 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,29 +1,40 @@
 const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
-
-require('dotenv').config();
-
-const middlewares = require('./middlewares');
-const api = require('./api');
-
+const { Pool } = require('pg');
+const pgp = require('pg-promise')(/* options */)
 const app = express();
 
-app.use(morgan('dev'));
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
+// PostgreSQL connection pool
+// const pool = new Pool({
+//   connectionString: 'psql "postgresql://bhargavjoshi1237:JtqLix7po8Ws@ep-weathered-frog-53534052.ap-southeast-1.aws.neon.tech/data?sslmode=require"',
+// });
 
-app.get('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
+
+
+// Define a route to perform the SELECT query
+app.get('/', async (req, res) => {
+  // try {
+  //   // Execute the SELECT query
+  //   const client = await pool.connect();
+  //   const queryResult = await client.query('SELECT * FROM data'); // Replace your_table_name with your actual table name
+  //   client.release();
+
+  //   // Send the query result as a JSON response
+  //   res.json(queryResult.rows);
+  // } catch (err) {
+  //   console.error('Error executing query:', err);
+  //   res.status(500).send(err);
+  // }
+
+const db = pgp('postgresql://bhargavjoshi1237:JtqLix7po8Ws@ep-weathered-frog-53534052.ap-southeast-1.aws.neon.tech/data?sslmode=require')
+
+db.one('SELECT * FROM data;')
+  .then((data) => {
+    console.log('DATA:', data)
+    res.json(data)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
 });
-
-app.use('/api/v1', api);
-
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
 
 module.exports = app;
