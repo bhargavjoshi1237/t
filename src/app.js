482d2680afe826079df848999485997d93ecee65
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2023-11-15 03:28:45 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const req = require('express/lib/request');
 const { Pool } = require('pg');
 const pgp = require('pg-promise')(/* options */)
 const app = express();
@@ -8,33 +9,62 @@ const app = express();
 //   connectionString: 'psql "postgresql://bhargavjoshi1237:JtqLix7po8Ws@ep-weathered-frog-53534052.ap-southeast-1.aws.neon.tech/data?sslmode=require"',
 // });
 
+app.get('/get/user/:username', (req, res) => {
+  const xop = req.params.username;
+  
+  if (xop) {
 
+  const db = pgp('postgresql://bhargavjoshi1237:JtqLix7po8Ws@ep-weathered-frog-53534052.ap-southeast-1.aws.neon.tech/data?sslmode=require')
+  db.one(`SELECT * FROM data  WHERE username = '${xop}';`)
+  .then((data) => {  
+    let op = JSON.parse(JSON.stringify(data))
+    res.json(op)
+  })
+  .catch((error) => {console.log('ERROR:', error) })}
+  
+  else {
+    res.send('Please provide a valid item name in the URL.');
+  }
+});
 
 // Define a route to perform the SELECT query
 app.get('/', async (req, res) => {
-  // try {
-  //   // Execute the SELECT query
-  //   const client = await pool.connect();
-  //   const queryResult = await client.query('SELECT * FROM data'); // Replace your_table_name with your actual table name
-  //   client.release();
-
-  //   // Send the query result as a JSON response
-  //   res.json(queryResult.rows);
-  // } catch (err) {
-  //   console.error('Error executing query:', err);
-  //   res.status(500).send(err);
-  // }
-
-const db = pgp('postgresql://bhargavjoshi1237:JtqLix7po8Ws@ep-weathered-frog-53534052.ap-southeast-1.aws.neon.tech/data?sslmode=require')
-
-db.one('SELECT * FROM data;')
-  .then((data) => {
-    console.log('DATA:', data)
-    res.json(data)
-  })
-  .catch((error) => {
-    console.log('ERROR:', error)
-  })
+res.json("hello")
 });
+
+// app.post('/', async (req, res) => {
+
+  
+
+// })
+
+app.get('/set/usert/:username/', async (req, res) => {
+  const name = req.params.username;  
+  const db = pgp('postgresql://bhargavjoshi1237:JtqLix7po8Ws@ep-weathered-frog-53534052.ap-southeast-1.aws.neon.tech/data?sslmode=require')
+  db.one(`INSERT INTO users (username, "Name", tickets, location, celsius, notifications, pro, pfp)
+  VALUES
+  ('${name}', '${name}', '{"empty": "empty"}', '[{"country": "USA"}, {"city": "New York"}]', true, false, true, 'https://i.ibb.co/zrw0zv1/pfp-modified.png') RETURNING * ;`)
+  .then((data) => {  
+    let op = JSON.parse(JSON.stringify(data))
+    res.json(op)
+  })
+  .catch((error) => {console.log('ERROR:', error) })
+});
+
+
+// app.get('/get/user/:username/', async (req, res) => {
+//   const name = req.params.username;  
+//   const db = pgp('postgresql://bhargavjoshi1237:JtqLix7po8Ws@ep-weathered-frog-53534052.ap-southeast-1.aws.neon.tech/data?sslmode=require')
+//   db.one(`SELECT * FROM user_profile  WHERE username = '${name}';`)
+//   .then((data) => {  
+//     let op = JSON.parse(JSON.stringify(data))
+//     res.json(op)
+//   })
+//   .catch((error) => {console.log('ERROR:', error) })
+// });
+
+
+
+
 
 module.exports = app;
