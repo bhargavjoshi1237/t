fe70ac324e9c848ce5459f5ec3d9edf9f544ebc7
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2023-11-27 10:55:31 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,21 @@ const req = require('express/lib/request');
 const { Pool } = require('pg');
 const pgp = require('pg-promise')(/* options */)
 const app = express();
+const mysql = require('mysql2/promise');
 
 // PostgreSQL connection pool
 // const pool = new Pool({
 //   connectionString: 'psql "postgresql://bhargavjoshi1237:JtqLix7po8Ws@ep-weathered-frog-53534052.ap-southeast-1.aws.neon.tech/data?sslmode=require"',
 // });
+const dbConfig = {
+  host: 'aws.connect.psdb.cloud',
+  user: 'ig7hn43hshskk2w14xy8',
+  password: 'pscale_pw_oYSMXweuL7ufvcuJtgSbwH4bop1RAjjYX3HmHDayiJa',
+  database: 'wss',
+  ssl: {
+    rejectUnauthorized: true
+  }
+};
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -77,6 +87,73 @@ app.get('/set/unit/:username/', async (req, res) => {
   })
   .catch((error) => {console.log('ERROR:', error) })
 });
+
+app.get('/get/ticket/:username',  async (req, res) => {
+  const name = req.params.username; 
+  try {
+    // Capture the start time
+    const startTime = new Date();
+
+    // Establish a connection to the database
+    const connection = await mysql.createConnection(dbConfig);
+
+    // Example query to select data from a table
+    const [rows, fields] = await connection.execute(`SELECT id FROM ticket_data where username = '${name}'`);
+
+    // Capture the end time
+    const endTime = new Date();
+
+    // Calculate the response time in milliseconds
+    const responseTime = endTime - startTime;
+
+    // Close the database connection
+    await connection.end();
+
+    // Log the response time
+    console.log(`Database response time: ${responseTime} ms`);
+
+    // Send the data as a JSON response
+    res.json(rows);
+  } catch (error) {
+    // Handle errors
+    console.error('Error fetching data from the database:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/get/ticket/location/:username', async (req, res) => {
+  const name = req.params.username;
+  try {
+    // Capture the start time
+    const startTime = new Date();
+
+    // Establish a connection to the database
+    const connection = await mysql.createConnection(dbConfig);
+
+    // Example query to select data based on the username
+    const [rows, fields] = await connection.execute(`SELECT id FROM ticket_data WHERE locname = '${name}'`);
+
+    // Capture the end time
+    const endTime = new Date();
+
+    // Calculate the response time in milliseconds
+    const responseTime = endTime - startTime;
+
+    // Close the database connection
+    await connection.end();
+
+    // Log the response time
+    console.log(`Database response time: ${responseTime} ms`);
+
+    // Send the data as a JSON response
+    res.json(rows);
+  } catch (error) {
+    // Handle errors
+    console.error('Error fetching data from the database:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 
 
 module.exports = app;
