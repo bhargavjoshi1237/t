23b06bd1be0f5f2682aea3618ec4ea829c56277d
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2024-01-28 22:01:56 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,26 +1,30 @@
 const express = require('express');
-const req = require('express/lib/request');
-const { Pool } = require('pg');
-const pgp = require('pg-promise')(/* options */)
-const app = express();
 const mysql = require('mysql2/promise');
-require('dotenv').config()
+const compression = require('compression');
+const { Pool } = require('pg');
+require('dotenv').config();
 
+const app = express();
+const port = process.env.PORT || 3000;
 
-
-
-const u = "7b6xnrvnqpjrz4prwxm7wwwww";
-const p = "pscale_pw_2qUIZ4tWFDr0ncaV7ExN8TVrK7amHyg5gKJ0tiKvau2wwwww";
-console.log(u.slice(0, -3))
 const dbConfig = {
   host: 'aws.connect.psdb.cloud',
-  user: u.slice(0, -5),
-  password: p.slice(0, -5),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: 'animes',
   ssl: {
     rejectUnauthorized: true
   }
 };
+
+const pool = mysql.createPool({
+  ...dbConfig,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+app.use(compression());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://wss-hqv8eibx9-bhargavjoshi1237.vercel.app');
@@ -33,47 +37,29 @@ app.use((req, res, next) => {
   next();
 });
 
-
 app.get('/', async (req, res) => {
-  res.json("hello")
-  console.log(process.env.PSU)
-  });
-  console.log(`MySQL username: ${u.slice(0, -5)}`);
-  console.log(`MySQL password: ${p.slice(0, -5)}`);
-  
+  res.json("hello");
+});
 
-app.get('/f/:username',  async (req, res) => {
-  const name = req.params.username; 
+app.get('/f/:username', async (req, res) => {
+  const name = req.params.username;
   try {
-    // Capture the start time
     const startTime = new Date();
 
-    // Establish a connection to the database
-    const connection = await mysql.createConnection(dbConfig);
+    const [rows, fields] = await pool.execute(`SELECT * FROM anime_raws WHERE anime_id = ?`, [name]);
 
-    // Example query to select data from a table
-    const [rows, fields] = await connection.execute(`SELECT * FROM anime_raws where anime_id = '${name}'`);
-
-    // Capture the end time
     const endTime = new Date();
-
-    // Calculate the response time in milliseconds
     const responseTime = endTime - startTime;
 
-    // Close the database connection
-    await connection.end();
-
-    // Log the response time
     console.log(`Database response time: ${responseTime} ms`);
 
-    // Send the data as a JSON response
     res.json(rows);
   } catch (error) {
-    // Handle errors
     console.error('Error fetching data from the database:', error);
     res.status(500).send('Internal Server Error');
   }
 });
 
-
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
