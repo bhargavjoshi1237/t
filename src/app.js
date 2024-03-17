7d691fa9fdbf6d7ed239b8b6fb89bf9cfcfe496a
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2024-03-17 18:18:31 +0000

    gg

--- src/app.js
+++ src/app.js
@@ -3,24 +3,27 @@ const req = require('express/lib/request');
 const { Pool } = require('pg');
 const pgp = require('pg-promise')(/* options */)
 const app = express();
+// var requests = require('requests');
 const mysql = require('mysql2/promise');
 require('dotenv').config()
+// const scrapeHtmlWeb = require('scrape-html-web'); // Assuming you have installed the scrape-html-web package
+const { scrapeHtmlWeb } = require("scrape-html-web");
 
 
+// const u = "7b6xnrvnqpjrz4prwxm7wwwww";
+// const p = "pscale_pw_2qUIZ4tWFDr0ncaV7ExN8TVrK7amHyg5gKJ0tiKvau2wwwww";
+// console.log(u.slice(0, -3))
+// const dbConfig = {
+//   host: 'aws.connect.psdb.cloud',
+//   user: u.slice(0, -5),
+//   password: p.slice(0, -5),
+//   database: 'animes',
+//   ssl: {
+//     rejectUnauthorized: true
+//   }
+// };
 
 
-const u = "7b6xnrvnqpjrz4prwxm7wwwww";
-const p = "pscale_pw_2qUIZ4tWFDr0ncaV7ExN8TVrK7amHyg5gKJ0tiKvau2wwwww";
-console.log(u.slice(0, -3))
-const dbConfig = {
-  host: 'aws.connect.psdb.cloud',
-  user: u.slice(0, -5),
-  password: p.slice(0, -5),
-  database: 'animes',
-  ssl: {
-    rejectUnauthorized: true
-  }
-};
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://wss-hqv8eibx9-bhargavjoshi1237.vercel.app');
@@ -34,70 +37,38 @@ app.use((req, res, next) => {
 });
 
 
-app.get('/', async (req, res) => {
-  res.json("hello")
-  console.log(process.env.PSU)
-  });
-  console.log(`MySQL username: ${u.slice(0, -5)}`);
-  console.log(`MySQL password: ${p.slice(0, -5)}`);
+// app.get('/', async (req, res) => {
+//   res.json("hello")
+//   console.log(process.env.PSU)
+//   });
+
   
-
-app.get('/f/:id',  async (req, res) => {
-  const name = req.params.id; 
-  try {
-    const startTime = new Date();
-    const connection = await mysql.createConnection(dbConfig);
-    const [rows, fields] = await connection.execute(`SELECT * FROM anime_raws where anime_id = '${name}'`);
-    const endTime = new Date();
-    const responseTime = endTime - startTime;
-    await connection.end();
-    console.log(`Database response time: ${responseTime} ms`);
-    res.json(rows);
-  } catch (error) {
-    console.error('Error fetching data from the database:', error);
-    res.status(500).send('Internal Server Error');
-  }
-}  );
-
-app.get('/al/:username',  async (req, res) => {
-  const name = req.params.username; 
-  try {
-    const startTime = new Date();
-    const connection = await mysql.createConnection(dbConfig);
-    const [rows, fields] = await connection.execute(`SELECT List FROM AnimeList where AnimeListID = '${name}'`);
-    const endTime = new Date();
-    const responseTime = endTime - startTime;
-    await connection.end();
-    console.log(`Database response time: ${responseTime} ms`);
-    res.json(rows);
-  } catch (error) {
-    console.error('Error fetching data from the database:', error);
-    res.status(500).send('Internal Server Error');
-  }
-}
-       
-       );
+  app.get('/', async (req, res) => {
+    try {
+      const options = {
+        url: "https://www.animenewsnetwork.com/news/",
+        bypassCors: true,
+        // list: true,
+        mainSelector: ".mainfeed-section ",
+        childrenSelector: [
+          { key: "date", selector: "div",type: "text" } ,
+          // { key: "link", selector: "text", attr: "href" } ,
+          // { key: "linkx", selector: "a",type: "text" } 
+        ],
+      };
+      
+      const data = await scrapeHtmlWeb(options);
+      console.log(data)
+     res.json(data)
+     
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
 
 
-app.get('/addal/:username/:aid',  async (req, res) => {
-  const name = req.params.username; 
-  const id = req.params.aid; 
-  try {
-    const startTime = new Date();
-    const connection = await mysql.createConnection(dbConfig);
-    const [rows, fields] = await connection.execute(`SELECT List FROM AnimeList where AnimeListID = '${name}'`);
-    const endTime = new Date();
-    const responseTime = endTime - startTime;
-    await connection.end();
-    console.log(`Database response time: ${responseTime} ms`);
-    res.json(rows);
-  } catch (error) {
-    console.error('Error fetching data from the database:', error);
-    res.status(500).send('Internal Server Error');
-  }
-}
-       
-       );
+
 
 
 
