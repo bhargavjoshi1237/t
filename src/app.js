3775d538fbca39f980b866059ae8478921208e75
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2024-03-19 12:59:11 +0000

    gg

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const req = require('express/lib/request');
-const { Pool } = require('pg');
-const pgp = require('pg-promise')(/* options */)
 const app = express();
-// var requests = require('requests');
-const mysql = require('mysql2/promise');
-require('dotenv').config()
-// const scrapeHtmlWeb = require('scrape-html-web'); // Assuming you have installed the scrape-html-web package
+const Redis = require("ioredis");
+
 const { scrapeHtmlWeb } = require("scrape-html-web");
+const client = new Redis("redis://default:553e61e7c64649478d1ff784688c052b@correct-stud-36874.upstash.io:36874");
+
 
 
 // const u = "7b6xnrvnqpjrz4prwxm7wwwww";
@@ -43,30 +40,103 @@ app.use((req, res, next) => {
 //   });
 
   
-  app.get('/', async (req, res) => {
-    try {
-      const options = {
-        url: "https://www.animenewsnetwork.com/news/",
-        bypassCors: true,
-        // list: true,
-        mainSelector: ".mainfeed-section ",
-        childrenSelector: [
-          { key: "date", selector: "div",type: "text" } ,
-          // { key: "link", selector: "text", attr: "href" } ,
-          // { key: "linkx", selector: "a",type: "text" } 
-        ],
-      };
-      
-      const data = await scrapeHtmlWeb(options);
-      console.log(data)
-     res.json(data)
-     
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  });
+app.get('/', async (req, res) => {
+  try {
+    // Record the start time
+    const startTime = new Date();
 
+    const options = {
+      url: "https://myanimelist.net/news",
+      list: true,
+      bypassCors: true,
+      mainSelector: ".news-list ",
+      childrenSelector: [
+        { key: "tags", selector: ".tags ", type: "text" },
+        { key: "title", selector: ".title", type: "text" },
+        { key: "writer", selector: ".info  ", type: "text" }, 
+        { key: "description", selector: ".text ", type: "text" },
+        { key: "imagex", selector: ".image ", attr: "src" },
+       
+
+        // { key: "image", selector: ".clearfix", type: "text" },
+
+       ],
+    };
+
+    // Perform the scraping operation
+    const data = await scrapeHtmlWeb(options);
+    console.log(data)
+    res.json(data.slice(0,20))
+    // Calculate the time taken
+    const endTime = new Date();
+    const elapsedTime = endTime - startTime; // Time difference in milliseconds
+
+    console.log('Time taken to fetch scrape data:', elapsedTime, 'ms');
+    await client.set('myanimelist_news', JSON.stringify(data.slice(0,20)));
+    // Send the data as response
+    
+
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+const scrapeAndStoreData = async () => {
+  try {
+    const response = await fetch('https://oga.onrender.com/');
+    if (!response.ok) {
+      throw new Error('Failed to fetch data from API');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data from API:', error);
+    throw error;
+  }
+  try {
+    const startTime = new Date();
+
+    const options = {
+      url: "https://myanimelist.net/news",
+      list: true,
+      bypassCors: true,
+      mainSelector: ".news-list ",
+      childrenSelector: [
+        { key: "tags", selector: ".tags ", type: "text" },
+        { key: "title", selector: ".title", type: "text" },
+        { key: "writer", selector: ".info  ", type: "text" }, 
+        { key: "description", selector: ".text ", type: "text" },
+        { key: "imagex", selector: ".image ", attr: "src" },
+      ],
+    };
+
+    // Perform the scraping operation
+    const data = await scrapeHtmlWeb(options);
+    
+    // Store data in Redis
+    await client.set('myanimelist_news', JSON.stringify(data.slice(0,20)));
+
+    // Calculate the time taken
+    const endTime = new Date();
+    const elapsedTime = endTime - startTime; // Time difference in milliseconds
+
+    console.log('Time taken to fetch scrape data:', elapsedTime, 'ms');
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+// Execute the function initially
+scrapeAndStoreData();
+
+// Set up a scheduler to execute the function every 10 minutes
+const intervalId = setInterval(scrapeAndStoreData, 1 * 60 * 1000);
+
+// Handle process termination to clear the interval
+process.on('SIGINT', () => {
+  clearInterval(intervalId);
+  console.log('Process terminated. Interval cleared.');
+});
 
 
 
