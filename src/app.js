843c7d31060f47832294e9bc259467b848186c3c
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2024-07-14 20:15:25 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,25 +1,5 @@
 const express = require('express');
 const app = express();
-const Redis = require("ioredis");
-const { Client } = require("pg");
-const { scrapeHtmlWeb } = require("scrape-html-web");
-const client = new Redis("redis://default:553e61e7c64649478d1ff784688c052b@correct-stud-36874.upstash.io:36874");
-const clientx = new Client("postgresql://jack:jy0LTlC8AlTandmUfJEv3A@wss-uesrs-3515.7s5.aws-ap-south-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full");
-
-
-
-// const u = "7b6xnrvnqpjrz4prwxm7wwwww";
-// const p = "pscale_pw_2qUIZ4tWFDr0ncaV7ExN8TVrK7amHyg5gKJ0tiKvau2wwwww";
-// console.log(u.slice(0, -3))
-// const dbConfig = {
-//   host: 'aws.connect.psdb.cloud',
-//   user: u.slice(0, -5),
-//   password: p.slice(0, -5),
-//   database: 'animes',
-//   ssl: {
-//     rejectUnauthorized: true
-//   }
-// };
 
 
 
@@ -35,56 +15,15 @@ app.use((req, res, next) => {
 });
 
 
-// app.get('/', async (req, res) => {
-//   res.json("hello")
-//   console.log(process.env.PSU)
-//   });
+app.get('/', async (req, res) => {
+  res.json("hello")
+  });
 
-app.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const start = Date.now(); // Record the start time
+app.get('/ping', async (req, res) => {
+  res.json("pong")
+  });
 
-  try {
-    // Fetch the username associated with the token from Redis
-    const username = await client.get(id);
-    res.json(JSON.parse(username));
 
-    const end = Date.now(); // Record the end time
-    const responseTime = end - start; // Calculate response time
-    console.log(`Response time: ${responseTime} ms`);
-  } catch (error) {
-    console.error('Error fetching username from Redis:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-
- 
-  (async () => {
-    try {
-      // Connect to the database
-      await clientx.connect();
-  
-      // Record the start time
-      const startx = Date.now();
-  
-      // Execute the query
-      const results = await clientx.query("SELECT * FROM users;");
-      console.log(results.rows);
-  
-      // Record the end time
-      const endx = Date.now();
-  
-      // Calculate response time
-      const responseTimex = endx - startx;
-      console.log(`Response time: ${responseTimex} ms`);
-  
-      // Close the database connection
-      await clientx.end();
-    } catch (err) {
-      // Handle errors
-      console.error("Error executing query:", err);
-    }
-  })();
-  
 
 });
 
